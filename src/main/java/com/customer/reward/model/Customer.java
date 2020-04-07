6eed3d0888cca0b4f4de4e05a88b8e33e383d340
rsred <rsred@192.168.0.173>
2020-04-07 11:30:17 -0500

    Summary: Added equals, hashcode methods to model objects

--- src/main/java/com/customer/reward/model/Customer.java
+++ src/main/java/com/customer/reward/model/Customer.java
@@ -32,6 +32,40 @@ public class Customer {
 	public void setCustomerTotalRewards(int customerTotalRewards) {
 		this.customerTotalRewards = customerTotalRewards;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((customerName == null) ? 0 : customerName.hashCode());
+		result = prime * result + customerTotalRewards;
+		result = prime * result + ((transactions == null) ? 0 : transactions.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (customerName == null) {
+			if (other.customerName != null)
+				return false;
+		} else if (!customerName.equals(other.customerName))
+			return false;
+		if (customerTotalRewards != other.customerTotalRewards)
+			return false;
+		if (transactions == null) {
+			if (other.transactions != null)
+				return false;
+		} else if (!transactions.equals(other.transactions))
+			return false;
+		return true;
+	}
 	
 
 }
--- src/main/java/com/customer/reward/model/Transaction.java
+++ src/main/java/com/customer/reward/model/Transaction.java
@@ -16,4 +16,27 @@ public class Transaction {
 		this.dollersSpent = dollersSpent;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + dollersSpent;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transaction other = (Transaction) obj;
+		if (dollersSpent != other.dollersSpent)
+			return false;
+		return true;
+	}
+	
+
 }
